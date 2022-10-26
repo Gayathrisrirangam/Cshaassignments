3f63eec1a248907253237b18eb822c50773ab391
Gayathrisrirangam <101495614+Gayathrisrirangam@users.noreply.github.com>
2022-10-26 19:32:15 +0530

    Add files via upload

---
+++ Ass1/Ass1/Ass2.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ass1
+{
+    internal class Ass2
+    {
+        public static void Main()
+        {
+            try
+            {
+                int[] marks = new int[5];
+                int sum = 0;
+                float average;
+                int highest = 0;
+                Console.WriteLine("Enetr Marks:");
+                for (int i = 0; i < marks.Length; i++)
+                {
+                    marks[i] = int.Parse(Console.ReadLine());
+                }
+                for (int i = 0; i < marks.Length; i++)
+                {
+                    sum += marks[i];
+                    if (marks[i] > highest)
+                    {
+                        highest = marks[i];
+                    }
+                }
+                average = sum / 5;
+                Console.WriteLine("Average = {0} Highest{1}", average, highest);
+
+            }
+            catch (Exception Error)
+            {
+                Console.WriteLine(Error.ToString());
+            }
+            finally
+            {
+                Console.WriteLine("Final");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Ass1/Ass1/add2num.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ass1
+{
+    internal class add2num
+    {
+        public static void main()
+        {
+            int a, b;
+            Console.WriteLine("Enter the Two Numbers : ");
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            if (a > b)
+            {
+                Console.WriteLine("{0} is the Greatest Number", a);
+            }
+            else
+            {
+                Console.WriteLine("{0} is the Greatest Number ", b);
+            }
+            Console.ReadLine();
+        }
+    }
+}
