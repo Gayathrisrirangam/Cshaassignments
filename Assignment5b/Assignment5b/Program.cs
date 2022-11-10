04a8738bfc83ca74ab2f9d14cb87d448736e5171
Gayathrisrirangam <101495614+Gayathrisrirangam@users.noreply.github.com>
2022-11-10 15:09:55 +0530

    Add files via upload

---
+++ Assignment5b/Assignment5b/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class EmployeeManagementSystem
+    {
+        public static void Main()
+        {
+
+            ArrayList Employee = new ArrayList();
+            Employee.Add(22);
+            Employee.Add("Gayathri");
+            Employee.Add("Trainee");
+            Employee.Add(20000);
+            Employee.Add("Banglore");
+
+
+
+            foreach (object i in Employee)
+            {
+                Console.WriteLine(i + " ");
+
+            }
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
